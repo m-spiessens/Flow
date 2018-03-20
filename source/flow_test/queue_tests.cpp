819eb988abcd29ece1886445f87003fbf3396644
m-spiessens <mathias@spiessens.eu>
2018-03-20 21:48:14 +0100

    Updated source headers.

--- source/flow_test/queue_tests.cpp
+++ source/flow_test/queue_tests.cpp
@@ -1,25 +1,25 @@
-/*
- The MIT License (MIT)
-
- Copyright (c) 2017 Cynara Krewe
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software, hardware and associated documentation files (the "Solution"), to deal
- in the Solution without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Solution, and to permit persons to whom the Solution is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Solution.
-
- THE SOLUTION IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOLUTION OR THE USE OR OTHER DEALINGS IN THE
- SOLUTION.
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Cynara Krewe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software, hardware and associated documentation files (the "Solution"), to deal
+ * in the Solution without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Solution, and to permit persons to whom the Solution is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Solution.
+ *
+ * THE SOLUTION IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOLUTION OR THE USE OR OTHER DEALINGS IN THE
+ * SOLUTION.
  */
 
 #include <stdint.h>
@@ -67,6 +67,16 @@ TEST(Queue_TestBench, IsEmptyAfterCreation)
 		CHECK(!unitUnderTest[i]->dequeue(response));
 		CHECK(!unitUnderTest[i]->peek(response));
 	}
+}
+
+TEST(Queue_TestBench, CopyConstructor)
+{
+	Queue<char> other(1);
+	other.enqueue('0');
+	Queue<char> some(2);
+	Queue<char> copied(other);
+	some = copied;
+	CHECK(some.isFull());
 }
 
 TEST(Queue_TestBench, EnqueueDequeueItem)
