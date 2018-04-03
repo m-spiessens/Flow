1b84a9e3359a07f4f797fe6ab8d9da894bf20376
m-spiessens <mathias@spiessens.eu>
2018-04-03 21:26:15 +0200

    Preparation for stable 1.5 release.

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile
 
 class Flow(ConanFile):
-   requires = "Flow/1.5@spiessensm/testing", "CppUTest/3.8@spiessensm/stable"
+   requires = "Flow/1.5@spiessensm/stable", "CppUTest/3.8@spiessensm/stable"
    
    def imports(self):
       self.copy("*.h")
