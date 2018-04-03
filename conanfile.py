39b54160e17f521b2c7571c54aee45c0481830c0
m-spiessens <mathias@spiessens.eu>
2018-04-03 20:29:04 +0200

    Experiment with Travis CI.

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile
 
 class ExampleUsingFlow(ConanFile):
-   requires = "Flow/1.5@spiessensm/stable"
+   requires = "Flow/1.4@spiessensm/stable"
    
    def imports(self):
       self.copy("*.h")
